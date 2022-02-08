7a32a4c19a551603e3bc1fc1a47dcb5d6715b90e
João Ferreira <joaovtf39@gmail.com>
2022-02-07 23:11:10 -0300

    requisito 4 OK

---
+++ desafio4.sql
@@ -0,0 +1,13 @@
+SELECT 
+	user.nome AS usuario,
+    IF(MAX(history.data_reproducao) >= '2021-01-01', 'Usuario Ativo','Usuario Inativo') AS condicao_usuario
+FROM 
+	SpotifyClone.usuario AS user
+INNER JOIN 
+	SpotifyClone.historico_reproducao AS history
+ON
+	user.usuario_id = history.usuario_id
+GROUP BY
+	user.nome
+ORDER BY
+	user.nome;

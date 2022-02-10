eb5c314d0936da00fcce75813de0f3e353e0772a
João Ferreira <joaovtf39@gmail.com>
2022-02-10 16:18:14 -0300

    requisito 9 ok

---
+++ desafio9.sql
@@ -0,0 +1,12 @@
+SELECT 
+	COUNT(history.usuario_id) AS quantidade_musicas_no_historico
+FROM 
+	SpotifyClone.historico_reproducao AS history
+INNER JOIN
+	SpotifyClone.usuario AS user
+ON
+	user.usuario_id = history.usuario_id
+WHERE 
+  user.nome = 'Bill'
+GROUP BY 
+	history.usuario_id;
